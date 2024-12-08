3980e0cdca41b7bab4412a6c500560c2f7369a5a
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2024-12-08 15:41:04 +0100

    fix about

--- src/pages/aboutPage/AboutPage.tsx
+++ src/pages/aboutPage/AboutPage.tsx
@@ -13,16 +13,13 @@ export default function AboutPage() {
 					<article className={style.catégory}>
 						<h3 className={style.h3}>{translations.aboutPage.devTitle}</h3>
 						<p className={style.text}>{translations.aboutPage.dev}</p>
-
 						<h3 className={style.h3}>{translations.aboutPage.manaTitle}</h3>
 						<p className={style.text}>{translations.aboutPage.management}</p>
 					</article>
 					<article className={style.catégory}>
 						<h3 className={style.h3}>{translations.aboutPage.lectureTitle}</h3>
 						<p className={style.h4}>{translations.aboutPage.bookOne}</p>
-
 						<p className={style.h4}>{translations.aboutPage.bookTwo}</p>
-
 						<p className={style.h4}>{translations.aboutPage.bookThree}</p>
 					</article>
 					<article className={style.catégory}>
