94eec3f0c90d23505a298dc31b56594ef5287a0c
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2024-12-18 20:21:38 +0100

    fix: design

--- src/pages/skillsPage/SkillsPage.tsx
+++ src/pages/skillsPage/SkillsPage.tsx
@@ -7,24 +7,27 @@ export default function SkillsPage() {
 	const { translations } = useTranslation();
 	return (
 		<>
-			<section className={style.skillsSection}>
-				<h2 className={style.skillsH2} id="skills">
-					{translations.homePage.mySkills}
-				</h2>
-				<ul className={style.skills}>
-					{skills.map((s) => (
-						<motion.li
-							initial={{ opacity: 0 }}
-							transition={{ duration: 0.7, delay: 0.3 }}
-							whileInView={{ opacity: 1 }}
-							key={s.id}
-							className={style.list}
-						>
-							{s.name}
-						</motion.li>
-					))}
-				</ul>
-			</section>
+			<div className={style.skillsPage}>
+				<h2 className={style.h2}>SKILLS</h2>
+				<section className={style.skillsSection}>
+					<h2 className={style.skillsH2} id="skills">
+						{translations.homePage.mySkills}
+					</h2>
+					<ul className={style.skills}>
+						{skills.map((s) => (
+							<motion.li
+								initial={{ opacity: 0 }}
+								transition={{ duration: 0.7, delay: 0.3 }}
+								whileInView={{ opacity: 1 }}
+								key={s.id}
+								className={style.list}
+							>
+								{s.name}
+							</motion.li>
+						))}
+					</ul>
+				</section>
+			</div>
 		</>
 	);
 }
